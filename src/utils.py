180aff48e5a9b19b10adb775f937b77c304926d2
obayram34 <oralbayram28@gmail.com>
2023-05-22 03:47:09 +0300

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
-import pickle
+#import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -23,18 +23,20 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test,models):
+def evaluate_models(X_train, y_train, X_test, y_test,models,param):
     try:
+       # optimized_model={}
         report={}
+        #report_best_params={}
 
         for i in range(len(list(models))):
             model=list(models.values())[i]
-            #para=param[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
-            #gs=GridSearchCV(model,para,cv=3)
-           # gs.fit(X_train,y_train)
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            #model.set_params(**gs.best_params_)
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)# Train model
 
             #model.fit(X_train,y_train) # train model
@@ -48,16 +50,18 @@ def evaluate_models(X_train, y_train, X_test, y_test,models):
             test_model_score=r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]]=test_model_score
-        
-        return report
+           # report_best_params[list(models.keys())[i]]=gs.best_params_
+           # optimized_model[list(models.keys())[i]]=model
+
+        return report    #, optimized_model
     
     except Exception as e:
         raise CustomException(e,sys)
 
     
-    def load_object(file_path):
-        try:
-            with open(file_path, 'rb') as file_obj:
-                return pickle.load(file_obj)
-        except Exception as e:
-            raise CustomException(e,sys)
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
